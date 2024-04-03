354b2b3977bc188c514bcb882203678053fdf7e5
Bruno Bandeira Fernandes <brunobandeirafernandes@gmail.com>
2024-04-03 10:18:25 -0300

    Create User

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+
+
+const usersRoutes = require('./users.routes')
+
+
+const routes = Router()
+
+
+// Rotas dos controllers
+routes.use('/users', usersRoutes)
+
+
+module.exports = routes
---
+++ src/routes/users.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+
+
+const UsersController = require('../controllers/UsersController')
+
+
+const usersRoutes = Router()
+
+
+// Controller
+const usersController = new UsersController()
+
+
+// Rotas
+usersRoutes.post('/create', usersController.create)
+
+
+// Exporta
+module.exports = usersRoutes
